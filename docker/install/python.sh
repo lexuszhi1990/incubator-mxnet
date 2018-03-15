262ced052d10fc772640b0183bc64a496e383871
david_fu <gandor@qq.com>
2018-03-15 15:04:00 +0800

    add python3.6

--- docker/install/python.sh
+++ docker/install/python.sh
@@ -19,10 +19,12 @@
 
 # install libraries for mxnet's python package on ubuntu
 
-apt-get update && apt-get install -y python-dev python3-dev
+add-apt-repository ppa:jonathonf/python-3.6
+apt-get update && apt-get install -y python-dev python3-dev python3.6
 
 # the version of the pip shipped with ubuntu may be too lower, install a recent version here
 cd /tmp && wget https://bootstrap.pypa.io/get-pip.py && python3 get-pip.py && python2 get-pip.py
 
 pip2 install nose pylint numpy nose-timer requests Pillow
-pip3 install nose pylint numpy nose-timer requests Pillow
+pip3.5 install nose pylint numpy nose-timer requests Pillow
+pip3.6 install nose pylint numpy nose-timer requests Pillow
